52f4a32ee2a4c77bfe4c5a0b4c1a6b200b71406c
Shiwang019 <154586601+Shiwang019@users.noreply.github.com>
2023-12-22 15:04:33 +0530

    RewardValue.java

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,1 +1,32 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    //Constructor that accepts cash value
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue= convertCashToMiles(cashValue);
+
+    }
+
+    //Constructor that accepts miles value
+    public RewardValue(int milesValue){
+        this.milesValue= milesValue;
+        this.cashValue= convertMilesToCash(milesValue);
+    }
+    private double convertMilesToCash(double milesValue){
+        return milesValue * 0.0035;
+    }
+    private double convertCashToMiles(double cashValue){
+        return cashValue / 0.0035;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
 
